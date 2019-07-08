c6afb99e512e1cc356c0c7eeb6a98b563157f2de
zubair <zubair@A006-00076.arbisoft.com>
2019-07-08 12:15:48 +0500

    initial project setup

---
+++ EmojiCountrySelector/EmojiCountrySelector/EmojiCountrySelector.h
@@ -0,0 +1,19 @@
+//
+//  EmojiCountrySelector.h
+//  EmojiCountrySelector
+//
+//  Created by zubair on 7/8/19.
+//  Copyright © 2019 zubair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmojiCountrySelector.
+FOUNDATION_EXPORT double EmojiCountrySelectorVersionNumber;
+
+//! Project version string for EmojiCountrySelector.
+FOUNDATION_EXPORT const unsigned char EmojiCountrySelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmojiCountrySelector/PublicHeader.h>
+
+
